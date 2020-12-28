16c2f683bcb095e27f7347a0960a41b16cdbc716
baha <tytenov99@gmail.com>
2020-12-28 19:06:37 +0600

    added nav controller

--- app/src/main/java/com/student/mvvmtodoapp/ui/MainActivity.kt
+++ app/src/main/java/com/student/mvvmtodoapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.student.mvvmtodoapp
+package com.student.mvvmtodoapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.student.mvvmtodoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/student/mvvmtodoapp/ui/tasks/TaskFragment.kt
@@ -0,0 +1,8 @@
+package com.student.mvvmtodoapp.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.student.mvvmtodoapp.R
+
+class TaskFragment : Fragment(R.layout.fragment_tasks) {
+
+}
