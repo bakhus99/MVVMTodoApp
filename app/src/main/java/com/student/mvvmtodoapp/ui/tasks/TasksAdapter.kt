bf5fff10993095fbc97a059c7f90216b5df473d9
baha <tytenov99@gmail.com>
2020-12-29 20:17:53 +0600

    added list adapter

---
+++ app/src/main/java/com/student/mvvmtodoapp/ui/tasks/TasksAdapter.kt
@@ -0,0 +1,46 @@
+package com.student.mvvmtodoapp.ui.tasks
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.student.mvvmtodoapp.data.Task
+import com.student.mvvmtodoapp.databinding.ItemTaskBinding
+
+class TasksAdapter : ListAdapter<Task, TasksAdapter.TasksViewHolder>(DiffCallback()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
+        val binding = ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TasksViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    class TasksViewHolder(private val binding: ItemTaskBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(task: Task) {
+            binding.apply {
+                tvTask.text = task.name
+                checkboxTask.isChecked = task.completed
+                tvTask.paint.isStrikeThruText = task.completed
+                labelPriority.isVisible = task.important
+            }
+
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task) =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean =
+            oldItem == newItem
+    }
+}
--- app/src/main/java/com/student/mvvmtodoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/student/mvvmtodoapp/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,13 @@ package com.student.mvvmtodoapp.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.student.mvvmtodoapp.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val  taskDao: TaskDao
 ) : ViewModel() {
+
+    val tasks = taskDao.getTask().asLiveData()
+
 }
