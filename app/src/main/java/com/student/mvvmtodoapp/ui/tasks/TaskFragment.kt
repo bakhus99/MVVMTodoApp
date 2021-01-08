70325dcffa3933613cf028ac07961a74ff4c62f1
baha <tytenov99@gmail.com>
2021-01-08 18:00:24 +0600

    added saving data

--- app/src/main/java/com/student/mvvmtodoapp/ui/tasks/TaskFragment.kt
+++ app/src/main/java/com/student/mvvmtodoapp/ui/tasks/TaskFragment.kt
@@ -8,14 +8,20 @@ import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.student.mvvmtodoapp.R
+import com.student.mvvmtodoapp.data.SortOrder
+import com.student.mvvmtodoapp.data.Task
 import com.student.mvvmtodoapp.databinding.FragmentTasksBinding
 import com.student.mvvmtodoapp.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class TaskFragment : Fragment(R.layout.fragment_tasks) {
+class TaskFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClickListener {
 
     private val viewModel: TasksViewModel by viewModels()
 
@@ -23,7 +29,7 @@ class TaskFragment : Fragment(R.layout.fragment_tasks) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentTasksBinding.bind(view)
-        val taskAdapter = TasksAdapter()
+        val taskAdapter = TasksAdapter(this)
 
         binding.apply {
             rvTasks.apply {
@@ -38,6 +44,14 @@ class TaskFragment : Fragment(R.layout.fragment_tasks) {
         setHasOptionsMenu(true)
     }
 
+    override fun onItemClick(task: Task) {
+        viewModel.onTaskSelected(task)
+    }
+
+    override fun onCheckBoxClick(task: Task, isChecked: Boolean) {
+        viewModel.onTaskChanged(task,isChecked)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_fragment_tasks, menu)
         val searchItem = menu.findItem(R.id.action_search)
@@ -46,22 +60,25 @@ class TaskFragment : Fragment(R.layout.fragment_tasks) {
         searchView.onQueryTextChanged {
             viewModel.searchQuery.value = it
         }
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed).isChecked =
+                viewModel.preferencesFlow.first().hideCompleted
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.sort_by_name -> {
-                viewModel.sortOrder.value = SortOrder.BY_NAME
+                viewModel.onSortOrderSelected(SortOrder.BY_NAME)
                 true
             }
             R.id.action_sort_by_day -> {
-                viewModel.sortOrder.value = SortOrder.BY_DATE
-
+                viewModel.onSortOrderSelected(SortOrder.BY_DATE)
                 true
             }
             R.id.action_hide_completed -> {
                 item.isChecked = !item.isChecked
-                viewModel.hideCompleted.value = item.isChecked
+                viewModel.onHideCompletedClick(item.isChecked)
                 true
             }
             R.id.action_deleted_completed -> {
--- app/src/main/java/com/student/mvvmtodoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/student/mvvmtodoapp/ui/tasks/TasksViewModel.kt
@@ -3,28 +3,43 @@ package com.student.mvvmtodoapp.ui.tasks
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.student.mvvmtodoapp.data.PreferencesManager
+import com.student.mvvmtodoapp.data.SortOrder
+import com.student.mvvmtodoapp.data.Task
 import com.student.mvvmtodoapp.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 
 class TasksViewModel @ViewModelInject constructor(
-    private val taskDao: TaskDao
+    private val taskDao: TaskDao,
+    private val preferencesManager: PreferencesManager
 ) : ViewModel() {
     val searchQuery = MutableStateFlow("")
-    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
-    val hideCompleted = MutableStateFlow(false)
+    val preferencesFlow = preferencesManager.preferencesFlow
     private val taskFlow = combine(
         searchQuery,
-        sortOrder,
-        hideCompleted
-    ){ query,sortOrder,hideCompleted ->
-        Triple(query,sortOrder,hideCompleted)
-    }.flatMapLatest { (query,sortOrder,hideCompleted) ->
-        taskDao.getTask(query,sortOrder,hideCompleted)
+        preferencesFlow
+    ) { query, filterPreferences ->
+        Pair(query, filterPreferences)
+    }.flatMapLatest { (query, filterPreferences) ->
+        taskDao.getTask(query, filterPreferences.sortOrder, filterPreferences.hideCompleted)
     }
     val tasks = taskFlow.asLiveData()
+    fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
 
+    fun onHideCompletedClick(hideCompleted: Boolean) = viewModelScope.launch {
+        preferencesManager.updateHideCompleted(hideCompleted)
+    }
+
+    fun onTaskSelected(task: Task) {}
+
+    fun onTaskChanged(task: Task,isChecked:Boolean) = viewModelScope.launch {
+        taskDao.update(task.copy(completed = isChecked))
+    }
 }
 
-enum class SortOrder { BY_NAME, BY_DATE }
